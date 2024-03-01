37f480b2d7a71713cf645b2ce84985acf57c3d16
Candela <gcandela894@gmail.com>
2024-03-01 18:20:33 -0300

    nuevo-estilo

--- codigo.js
+++ codigo.js
@@ -9,9 +9,16 @@ let audio
 for(var i = 0; i < playCancion.length; i++){
     playCancion[i].addEventListener('click', function(){
         let cancion = this.getAttribute('id')
-        audio = new Audio(`./audios/${cancion}.mp3`)
-        audio.play()
-    })
+        //verificamos primero si no está sonando una...
+        if (audio && !audio.paused) {
+            audio.pause();
+            audio.currentTime = 0; // Reinicia la reproducción
+          }
+      
+          // Crear una nueva instancia de Audio para la canción seleccionada
+          audio = new Audio(`./audios/${cancion}.mp3`);
+          audio.play();
+     });
 }
 
 for(var i = 0; i < stopCancion.length; i++){
@@ -24,3 +31,19 @@ volumen.addEventListener('click', function(){
     let vol = this.value
     audio.volume = vol
 })
+
+
+
+/* Código de als flechas */
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Función para desplazar hacia la izquierda
+    document.querySelector(".arrow-left").addEventListener("click", function() {
+      document.getElementById("gallery-container").scrollLeft -= 200;
+    });
+
+    // Función para desplazar hacia la derecha
+    document.querySelector(".arrow-right").addEventListener("click", function() {
+      document.getElementById("gallery-container").scrollLeft += 200;
+    });
+  });
